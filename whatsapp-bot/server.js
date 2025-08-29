857ad07cfc612cebe360a2a8a161a31aa429eb17
Badmus Qudus Ayomide <badmusqudusayomide@gmail.com>
2025-08-29 02:55:09 +0100

    don

---
+++ whatsapp-bot/server.js
@@ -0,0 +1,7 @@
+// Server entry point for Render deployment
+// This file redirects to the main WhatsApp bot logic
+
+import './index.js'
+
+console.log('🚀 JARVIS WhatsApp Bot Server Started')
+console.log('📱 Bot is running and ready to scan QR code')
